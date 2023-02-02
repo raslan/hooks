20706ab5f091e58b06c582b4719905715c557516
raslan <24810123+raslan@users.noreply.github.com>
2023-02-02 20:47:31 +0200

    feat: add interval and timeout hooks

---
+++ src/useInterval/index.ts
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export const useInterval = (
+  callback: Function,
+  delay: number
+): {
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+} => {
+  const savedCallback = useRef<Function | null>(null);
+  const [running, setRunning] = useState(false);
+  const [time, setTime] = useState(delay);
+  const [update, forceUpdate] = useState(0);
+
+  const start = useCallback(() => setRunning(true), []);
+  const stop = useCallback(() => setRunning(false), []);
+  const reset = useCallback(() => {
+    setTime(delay);
+    forceUpdate((update) => update + 1);
+  }, [delay]);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    setTime(delay);
+  }, [delay]);
+
+  useEffect(() => {
+    const tick = () => savedCallback.current?.();
+    if (delay !== null && running) {
+      const id = window.setInterval(tick, time);
+      return () => window.clearInterval(id);
+    }
+    return () => {};
+  }, [delay, time, running, update]);
+
+  return { start, stop, reset };
+};
