a45e1ea6f723dea39cf1e14a832f2d4cc4878d6f
raslan <24810123+raslan@users.noreply.github.com>
2023-02-02 20:47:38 +0200

    feat: add useInOut

---
+++ src/useInOut/index.ts
@@ -0,0 +1,31 @@
+import { useCallback, useRef, useState } from 'react';
+import { useTimeout } from '../useTimeout/index';
+
+export const useInOut = (
+  [firstCallback, secondCallback]: [Function, Function],
+  delay: number
+): {
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+} => {
+  const initialCallback = useRef<Function>(firstCallback);
+  const endingCallback = useRef<Function>(secondCallback);
+  const [internalDelay, setInternalDelay] = useState(delay);
+
+  const { start: timeoutStart, reset } = useTimeout(
+    endingCallback.current,
+    internalDelay
+  );
+
+  const start = useCallback(() => {
+    timeoutStart();
+    setTimeout(initialCallback.current, 0);
+  }, []);
+
+  const stop = useCallback(() => {
+    setInternalDelay(0);
+  }, []);
+
+  return { start, stop, reset };
+};
