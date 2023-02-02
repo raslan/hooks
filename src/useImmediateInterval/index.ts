99da68602a1d5500e544f73457c7c9a2335f2457
raslan <24810123+raslan@users.noreply.github.com>
2023-02-02 20:47:22 +0200

    feat: add useImmediate hooks

---
+++ src/useImmediateInterval/index.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export const useImmediateInterval = (
+  callback: Function,
+  delay: number
+): {
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+} => {
+  const savedCallback = useRef<Function | null>(null);
+  const [running, setRunning] = useState(false);
+  const [time, setTime] = useState(delay);
+  const [update, forceUpdate] = useState(0);
+
+  const start = useCallback(() => setRunning(true), []);
+  const stop = useCallback(() => setRunning(false), []);
+  const reset = useCallback(() => {
+    setTime(delay);
+    forceUpdate((update) => update + 1);
+  }, [delay]);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    setTime(delay);
+  }, [delay]);
+
+  useEffect(() => {
+    const tick = () => savedCallback.current?.();
+    if (delay !== null && running) {
+      tick();
+      const id = window.setInterval(tick, time);
+      return () => window.clearInterval(id);
+    }
+    return () => {};
+  }, [delay, time, running, update]);
+
+  return { start, stop, reset };
+};
