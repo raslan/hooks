d276bbc34e58520d1020ff5f182b0b83f8f09b1e
raslan <24810123+raslan@users.noreply.github.com>
2023-02-02 20:46:53 +0200

    feat: add useCountdown hook

---
+++ src/useCountdown/index.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useInterval } from '../useInterval/index';
+
+export const useCountdown = (
+  inputDelay: number
+): {
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+  currentTime: number;
+} => {
+  const [currentTime, setCurrentTime] = useState(inputDelay);
+
+  const {
+    start,
+    stop,
+    reset: resetInterval,
+  } = useInterval(() => {
+    setCurrentTime(currentTime - 1000);
+  }, 1000);
+
+  const reset = useCallback(() => {
+    resetInterval();
+    setCurrentTime(inputDelay);
+  }, [inputDelay]);
+
+  useEffect(() => {
+    if (currentTime === 0) stop();
+  }, [currentTime]);
+
+  useEffect(() => {
+    setCurrentTime(inputDelay);
+  }, [inputDelay]);
+
+  return { start, stop, reset, currentTime };
+};
